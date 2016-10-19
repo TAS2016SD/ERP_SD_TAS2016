046acdc42826a7b57bb2d35582a9fbe6491fe5d2
Mateusz Dajerling <mateusz.dajerling@gmail.com>
2016-10-19 11:09:06 +0200

    added foreign  keys

--- Database4/Faktura_nagłówek.sql
+++ Database4/Faktura_nagłówek.sql
@@ -3,5 +3,5 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Płatnik] INT NOT NULL, 
     [Wystawiający] NCHAR(30) NOT NULL, 
-    [Data_wystawienia] DATETIME NOT NULL
-)
+    [Data_wystawienia] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Faktura_nagłówek_ToKlienci] FOREIGN KEY ([Płatnik]) REFERENCES [Klienci]([Id]))
--- Database4/ZleceniaNag.sql
+++ Database4/ZleceniaNag.sql
@@ -5,7 +5,11 @@
     [Id_platnika] INT NULL, 
     [Id_dostawcy] INT NOT NULL, 
     [Status] NCHAR(15) NOT NULL, 
-    [Wartość zlecenia] MONEY NOT NULL, 
+    [Wartosc zlecenia] MONEY NOT NULL, 
     [Data_utworzenia] DATE NOT NULL, 
-    [Tworzący] NCHAR(40) NOT NULL
+    [Tworzący] NCHAR(40) NOT NULL, 
+    CONSTRAINT [FK_ZleceniaNag_Klienci] FOREIGN KEY ([Id_odbiorcy]) REFERENCES [Klienci]([Id]),
+	CONSTRAINT [FK_ZleceniaNag_Klienci2] FOREIGN KEY ([Id_platnika]) REFERENCES [Klienci]([Id]), 
+    CONSTRAINT [FK_ZleceniaNag_ToDostawcy] FOREIGN KEY ([Id_dostawcy]) REFERENCES [Dostawcy]([Id])
+
 )
