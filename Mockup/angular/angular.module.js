7289968928cda02206f14ccca3262b0028e09f70
konikowska <katarzyna.konikowska@gmail.com>
2016-10-22 12:04:52 +0200

    angular battlefield

---
+++ Mockup/angular/angular.module.js
@@ -0,0 +1,36 @@
+//var App = angular.module('app', ['ui.router']);
+var App = angular.module('app', ['ui.router']);
+//var myApp = angular.module('myApp', ['ui.router','myApp.filters', 'myApp.services', 'myApp.directives', 'ngSanitize']);
+App.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('basicData', {
+      url: '/basicData',
+      views: {
+        nav: {
+          templateUrl: 'navbar.html'
+        },
+        content: {
+          templateUrl: 'basicData.html'
+        }
+      }
+    })
+    .state('warehouse', {
+      url: '/warehouse',
+      views: {
+        nav: {
+          templateUrl: 'navbar.html'
+        },
+        content: {
+          templateUrl: 'warehouse.html'
+        }
+      } 
+    }).state('landingpage', {
+      url: '/landingpage',
+      views: {
+        content: {
+          templateUrl: 'landingpage.html'
+        }
+      } 
+    });
+  $urlRouterProvider.otherwise('/basicData');
+});
