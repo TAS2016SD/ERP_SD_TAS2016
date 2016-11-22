ad4a9da6f58c98d03bb4e91a18a97699b1a705ef
Mateusz Dajerling <mateusz.dajerling@gmail.com>
2016-11-22 21:29:37 +0100

    Bla bla

--- Database4/Status_magazynu.sql
+++ Database4/Status_magazynu.sql
@@ -3,6 +3,8 @@
 	[Miejsce_skladowania] NCHAR(10) NOT NULL, 
     [Id_Materialu] INT NULL, 
     [Ilość] FLOAT NULL, 
-    CONSTRAINT [PK_Table1] PRIMARY KEY ([Miejsce_skladowania]), 
-    CONSTRAINT [FK_Status_magazynuToMaterialy] FOREIGN KEY ([Id_Materialu]) REFERENCES [Materialy]([Id]) 
+    CONSTRAINT [PK_Stan_magazynu] PRIMARY KEY ([Miejsce_skladowania]), 
+    CONSTRAINT [FK_Stan_magazynu_To] FOREIGN KEY ([Id_Materialu]) REFERENCES [Materialy]([Id]) ,
+
+    
 )
