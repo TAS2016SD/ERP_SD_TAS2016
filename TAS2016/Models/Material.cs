ce3d308101c15dd22bd9e45430c743e21add83a6
Mateusz Dajerling <mateusz.dajerling@gmail.com>
2017-01-11 10:56:23 +0100

    add Material model

---
+++ TAS2016/Models/Material.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TAS2016.Models
+{
+    public class Material
+    {
+        [Required]
+        [Key]
+        int Id { get; set; }
+        [Required]
+        String Name { get; set; }
+        [Required]
+        Decimal BasePrice { get; set; }
+        String  PriceUnit { get; set; }
+        int    Lenght { get; set; }
+        int    Width { get; set; }
+        int    Height { get; set; }
+        int    Weight { get; set; }
+    }
+}
