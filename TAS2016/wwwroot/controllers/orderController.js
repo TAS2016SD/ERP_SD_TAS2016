2dcc2f0835615320976ad7cac1d5bd101417bac3
konikowska <katarzyna.konikowska@gmail.com>
2017-02-01 17:12:37 +0100

    orders

--- TAS2016/wwwroot/controllers/orderController.js
+++ TAS2016/wwwroot/controllers/orderController.js
@@ -1,4 +1,4 @@
-app.controller('OrderController', function($scope)
+app.controller('OrderController', function($scope, $http)
 {
     $scope.test = 'TEST CONTROLLERA ZLECEŃ';
     $scope.headers = ["Id odbiorcy", "Id wystawcy", "Data utworzenia", "Tworzący"];
@@ -7,4 +7,21 @@ app.controller('OrderController', function($scope)
     $scope.modelNamesForBodies = ["Material", "Cena"];
     $scope.headersDisplay = ["Id","Id odbiorcy", "Id wystawcy", "Status", "Wartość zamównienia", "Data utworzenia", "Tworzący"];
     $scope.bodiesDisplay=["Id","Materiał","Ilość","Cena jednostkowa", "Cena za całość", "Cena po rabacie"];
+    console.log($scope.test);
+    $scope.pos=["id","amount","material","materialId","price"];
+    $scope.init = function(index) 
+    {
+        $scope.position = $scope.orders[index].positions[index];
+    }
+    $http({
+    method: 'GET',
+    url: 'http://localhost:61690/order'})
+    .then(function successCallback(response) {
+        console.log(response.data);
+        $scope.orders = response.data;
+    }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+        });
+    
 });
